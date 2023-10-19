2ffc9149fb008333872c0eeb730674a2d5c2260b
Ahmed Ibrahim <148149151+AhmedIbrahim812@users.noreply.github.com>
2023-10-19 23:17:47 +0300

    Update main.c

--- LCD/main.c
+++ LCD/main.c
@@ -10,40 +10,6 @@
 
 void main (void)
 {
-
-	DIO_voidSetPortDir(PORTD_REG,PORT_DIR_OUT);
-	DIO_voidSetPortDir(PORTC_REG,PORT_DIR_OUT);
-	CLCD_voidInit();
-
-//u8 arr[5]={'A','B'};
-//CLCD_voidSendData('A');
-
-	CLCD_voidSendNum(5);
-CLCD_voidSetPosition(0,1);
-	CLCD_voidSendString("Saif");
-//	CLCD_voidSendString("ENG.");
-//	CLCD_voidSetPosition(1,8);
-//	CLCD_voidSendString("Sief");
-	//u8 arr[8]={0b0001111,0b0001111,0b0000000,0b0001111,0b0001111,0b0000000,0b0001111,0b0001111};
-	//CLCD_voidDrawSpecialChar(arr, 1,0,3);
-
-
-while(1){
-
+// Here you can test the driver
+// Just write your code using implemented functions, and HAVE FUN!
 }
-
-
-
-
-}
-
-
-
-
-
-
-
-
-
-
-
