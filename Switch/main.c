47ff88dfd6f29b7a28ef1b921c9aa5920276065b
Ahmed Ibrahim <148149151+AhmedIbrahim812@users.noreply.github.com>
2023-10-19 23:18:10 +0300

    Update main.c

--- Switch/main.c
+++ Switch/main.c
@@ -6,13 +6,8 @@
 
 void main (void)
 {
-	//_delay_ms(500);
-	SW_t strct={PORTA_REG,PIN0,PIN_VAL_HIGH};
-	//SW_voidInit(strct);
-	u8 result= SW_u8GetPressed(strct);
-	//if (result==1)
-	DIO_voidSetPinVal(PORTD_REG,PIN0,result);
-
+// Here you can test the driver
+// Just write your code using implemented functions, and HAVE FUN!
 
 }
 
