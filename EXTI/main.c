3057e3941adc0235884e1b250dc65af39167975a
Ahmed Ibrahim <148149151+AhmedIbrahim812@users.noreply.github.com>
2023-10-19 23:17:13 +0300

    Update main.c

--- EXTI/main.c
+++ EXTI/main.c
@@ -6,37 +6,11 @@
 #include "EXTI_interface.h"
 
 
-void func(void);
 
 void main (void)
 {
-	DIO_voidSetPinDir(PORTD_REG,PIN2,PIN_DIR_IN);
-	//enable int0
-
-
-	DIO_voidSetPinDir(PORTA_REG,PIN0,PIN_DIR_OUT);
-	DIO_voidSetPinDir(PORTA_REG,PIN7,PIN_DIR_OUT);
-	DIO_voidSetPinVal(PORTA_REG,PIN7,PIN_VAL_HIGH);
-	DIO_voidSetPinVal(PORTA_REG,PIN0,PIN_VAL_HIGH);
-
-	EXTI_voidSetSenseCtrl(falling_edge,int0);
-	EXTI_voidEnableInterrupt(int0);
-	EXTI_voidSetCallBack(int0,&func);
-	GIE_voidEnable();
-	_delay_ms(2000);
-	DIO_voidSetPinVal(PORTD_REG,PIN2,PIN_VAL_HIGH);
-
-
-
-
-}
-
-
-void func(void)
-{
-	DIO_voidSetPinVal(PORTA_REG,PIN7,PIN_VAL_LOW);
-	DIO_voidSetPinVal(PORTA_REG,PIN0,PIN_VAL_LOW);
-		_delay_ms(2000);
+// Here you can test the driver
+// Just write your code using implemented functions, and HAVE FUN!
 }
 
 
@@ -44,34 +18,6 @@ void func(void)
 
 
 
-
-	/*
-	DIO_voidSetPinDir(PORTD_REG,PIN2,PIN_DIR_IN);
-
-	DIO_voidSetPinVal(PORTD_REG,PIN2,PIN_VAL_HIGH);
-	DIO_voidSetPinDir(PORTA_REG,PIN0,PIN_DIR_OUT);
-	DIO_voidSetPinDir(PORTA_REG,PIN7,PIN_DIR_OUT);
-
-	EXTI0_voidInit();
-	GIE_voidEnable();
-
-while (1){
-
-	DIO_voidSetPinVal(PORTA_REG,PIN7,PIN_VAL_HIGH);
-	DIO_voidSetPinVal(PORTA_REG,PIN0,PIN_VAL_HIGH);
-
-}
-
-
-}
-
-void __vector_1(void)  __attribute__((signal));
-void __vector_1(void)
-{
-	DIO_voidSetPinVal(PORTA_REG,PIN0,PIN_VAL_LOW);
-	_delay_ms(5000);
-
-*/
 
 
 
