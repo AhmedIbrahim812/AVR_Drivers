4098f1906c4b3fbe3ed76a48c7f3d1914337333b
Ahmed Ibrahim <148149151+AhmedIbrahim812@users.noreply.github.com>
2023-10-22 02:55:31 +0300

    Add comments to main.c

--- SPI/main.c
+++ SPI/main.c
@@ -12,26 +12,22 @@
 
 
 int main (void){
-
+	//MOSI
 	DIO_voidSetPinDir(PORTB_REG,PIN5,PIN_DIR_OUT);
+	//SS
 	DIO_voidSetPinDir(PORTB_REG,PIN4,PIN_DIR_OUT);
+	//SS is low
 	DIO_voidSetPinVal(PORTB_REG,PIN4,PIN_VAL_LOW);
-
+	//MISO
 	DIO_voidSetPinDir(PORTB_REG,PIN6,PIN_DIR_IN);
+	//Clock
 	DIO_voidSetPinDir(PORTB_REG,PIN7,PIN_DIR_OUT);
-	//DIO_voidSetPinVal(PORTB_REG,PIN7,PIN_VAL_LOW);
 
-
-SPI_voidIntMstr();
-while(1){
-
+	//initilise ECU as master
+	SPI_voidIntMstr();
+while(1)
+{
 	SPI_u8Transeve(5);
 }
-
-
-
-
-
-
 
 }
